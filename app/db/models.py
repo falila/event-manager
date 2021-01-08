f3d7499421ac94044116c7dd6927e805332f6965
falila <soulfa23@gmail.cm>
2021-01-08 00:58:44 -0500

    added routes, authorization

--- app/db/models.py
+++
@@ -1,9 +0,0 @@
-from .db import db 
-import datetime
-
-class Event(db.Document):
-    kind = db.StringField()
-    start_time = db.StringField()
-    end_time = db.StringField()
-    timestamp = db.DateTimeField(default=datetime.datetime.now())
-    
